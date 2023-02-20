b032d775958cd39b82d385d2092b6652af5cd614
HelHeim111 <dwp2004@icloud.com>
2023-02-20 19:31:35 +0100

    expanded white box tests

--- white_box_tests.cpp
+++ white_box_tests.cpp
@@ -32,4 +32,83 @@
 //       indexu
 //============================================================================//
 
+TEST(HashMap, Ctor) {
+    hash_map* tmpHash = hash_map_ctor();
+    ASSERT_TRUE(NULL != tmpHash);
+    hash_map_dtor(tmpHash);
+}
+
+TEST(HashMap, Reserved) {
+    hash_map* tmpHash = hash_map_ctor();
+    hash_map_state_code_t var = hash_map_reserve(tmpHash, 18);
+    ASSERT_TRUE(OK == var);
+    hash_map_put(tmpHash, "key1", 44);
+    var = hash_map_reserve(tmpHash, 18);
+    ASSERT_TRUE(OK == var);
+    hash_map_dtor(tmpHash);
+}
+
+TEST(HashMap, Put) {
+    hash_map* tmpHash = hash_map_ctor();
+    hash_map_state_code_t var = hash_map_put(tmpHash, "key1", 44);
+    ASSERT_TRUE(OK == var);
+    var = hash_map_put(tmpHash, "key1", 45);
+    ASSERT_TRUE(KEY_ALREADY_EXISTS == var);
+    hash_map_dtor(tmpHash);
+}
+
+TEST(HashMap, Size) {
+    hash_map* tmpHash = hash_map_ctor();
+    size_t tmpSize = hash_map_size(tmpHash);
+    ASSERT_TRUE(tmpSize == tmpHash->used);
+    hash_map_put(tmpHash, "key1", 44);
+    tmpSize = hash_map_size(tmpHash);
+    ASSERT_TRUE(tmpSize == tmpHash->used);
+    hash_map_dtor(tmpHash);
+}
+
+TEST(HashMap, Capacity) {
+    hash_map* tmpHash = hash_map_ctor();
+    size_t tmpSize = hash_map_capacity(tmpHash);
+    ASSERT_TRUE(tmpSize == tmpHash->allocated);
+    hash_map_put(tmpHash, "key1", 44);
+    tmpSize = hash_map_capacity(tmpHash);
+    ASSERT_TRUE(tmpSize == tmpHash->allocated);
+    hash_map_dtor(tmpHash);
+}
+
+TEST(HashMap, Contains) {
+    hash_map* tmpHash = hash_map_ctor();
+    bool val = hash_map_contains(tmpHash, "key1");
+    ASSERT_TRUE(!val);
+    hash_map_put(tmpHash, "key1", 44);
+    val = hash_map_contains(tmpHash, "key1");
+    ASSERT_TRUE(val);
+    hash_map_dtor(tmpHash);
+}
+
+TEST(HashMap, Get) {
+    hash_map* tmpHash = hash_map_ctor();
+    int* a = (int*)malloc(sizeof(int));
+    hash_map_state_code_t var = hash_map_get(tmpHash, "key1", a);
+    ASSERT_TRUE(var == KEY_ERROR);
+    hash_map_put(tmpHash, "key1", 44);
+    var = hash_map_get(tmpHash, "key1", a);
+    ASSERT_TRUE(var == OK);
+    free(a);
+    hash_map_dtor(tmpHash);
+}
+
+TEST(HashMap, Pop) {
+    hash_map* tmpHash = hash_map_ctor();
+    int* a = (int*)malloc(sizeof(int));
+    hash_map_state_code_t var = hash_map_pop(tmpHash, "key1", a);
+    ASSERT_TRUE(var == KEY_ERROR);
+    hash_map_put(tmpHash, "key1", 44);
+    var = hash_map_pop(tmpHash, "key1", a);
+    ASSERT_TRUE(var == OK);
+    free(a);
+    hash_map_dtor(tmpHash);
+}
+
 /*** Konec souboru white_box_tests.cpp ***/
